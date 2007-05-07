d17e01e8140e3b11bc62605bbe10af0d0fa3d785
sartak <sartak@gmail.com>
2007-05-06 22:23:50 -0500

    Initial version of interhack.pl, what we plan to do is pretty do-able

---
+++ interhack.pl
@@ -0,0 +1,109 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Term::ReadKey;
+use IO::Socket;
+
+# CONFIG
+my $nick = 'Interhack';
+my $pass = 'kool'; # zomg!
+my $killswitch = "\t"; # automatically kills the script, Just In Case
+# END CONFIG
+
+my $server = 'nethack.alt.org';
+
+my $sock = new IO::Socket::INET(PeerAddr => $server,
+                                PeerPort => 23,
+                                Proto => 'tcp');
+die "Could not create socket: $!\n" unless $sock;
+$sock->blocking(0);
+
+my $IAC = chr(255);
+my $SB = chr(250);
+my $SE = chr(240);
+my $WILL = chr(251);
+my $WONT = chr(252);
+my $DO = chr(253);
+my $DONT = chr(254);
+my $TTYPE = chr(24);
+my $TSPEED = chr(32);
+my $XDISPLOC = chr(35);
+my $NEWENVIRON = chr(39);
+my $IS = chr(0);
+my $GOAHEAD = chr(3);
+my $ECHO = chr(1);
+my $NAWS = chr(31);
+my $STATUS = chr(5);
+my $LFLOW = chr(33);
+my $LINEMODE = chr(34);
+
+print {$sock}"$IAC$WILL$TTYPE"
+            ."$IAC$SB$TTYPE${IS}xterm-color$IAC$SE"
+            ."$IAC$WONT$TSPEED"
+            ."$IAC$WONT$XDISPLOC"
+            ."$IAC$WONT$NEWENVIRON"
+            ."$IAC$DONT$GOAHEAD"
+            ."$IAC$WILL$ECHO"
+            ."$IAC$DO$STATUS"
+            ."$IAC$WILL$LFLOW"
+            ."$IAC$WILL$NAWS"
+            ."$IAC$SB$NAWS$IS".chr(80).$IS.chr(24)."$IAC$SE";
+
+# autologin
+print {$sock} "l$nick\n$pass\n";
+
+ReadMode 3;
+END { ReadMode 0 }
+$|++;
+
+my $buf;
+
+while (1)
+{
+  # read from stdin, print to sock
+  if (defined(my $c = ReadKey -1))
+  {
+    last if $c eq $killswitch;
+    print {$sock} $c;
+  }
+
+  # read from sock, print to stdout
+  if (defined(recv($sock, $buf, 1024, 0)))
+  {
+    # example of a simple message coloring
+    $buf =~ s{The newt bites.}{\e[1;31m$&\e[0m}g;
+
+    # make floating eyes bright cyan
+    $buf =~ s{\e\[(?:0;)?34me}{\e[0;36me}g;
+
+    # make jackals look like mind flayers
+    $buf =~ s{\e\[(?:0;)?33md}{\e[0;35mh}g;
+    $buf =~ s{\bjackal\b}{master mind flayer}g;
+
+    # colorify picking up gold
+    $buf =~ s{H\d+ gold pieces?\.}{\e[1;33m$&\e[0m}g;
+
+    # power colors!
+    $buf =~ s{Pw:((-?\d+)\((-?\d+)\))}{
+      my $color = '';
+
+      if ($2 >= $3) { }
+      elsif ($2 * 2 >= $3)
+      {
+        $color = "\e[1;36m";
+      }
+      elsif ($2 * 3 >= $3)
+      {
+        $color = "\e[1;35m";
+      }
+      else
+      {
+        $color = "\e[0;35m";
+      }
+
+      "Pw:$color$1\e[0m"
+      }eg;
+    print $buf;
+  }
+}
+
