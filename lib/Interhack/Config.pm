4fcef93184dadb41295d7475fc1ee870612d42fe
sartak <sartak@gmail.com>
2007-05-11 17:48:12 -0500

    Move colormap into interhack.pl for now

--- lib/Interhack/Config.pm
+++ lib/Interhack/Config.pm
@@ -3,60 +3,6 @@ package Interhack::Config;
 use strict;
 use warnings;
 
-my %colormap =
-(
-  black          => "\e[0;30m",
-  bblack         => "\e[1;30m",
-  "bold&black"   => "\e[1;30m",
-  "black&bold"   => "\e[1;30m",
-  bblack         => "\e[1;30m",
-  darkgray       => "\e[1;30m",
-  darkgrey       => "\e[1;30m",
-
-  red            => "\e[0;31m",
-  bred           => "\e[1;31m",
-  "bold&red"     => "\e[1;31m",
-  "red&bold"     => "\e[1;31m",
-  orange         => "\e[1;31m",
-
-  green          => "\e[0;32m",
-  bgreen         => "\e[1;32m",
-  "bold&green"   => "\e[1;32m",
-  "green&bold"   => "\e[1;32m",
-
-  brown          => "\e[0;33m",
-  bbrown         => "\e[1;33m",
-  "bold&brown"   => "\e[1;33m",
-  "brown&bold"   => "\e[1;33m",
-  yellow         => "\e[1;33m",
-  darkyellow     => "\e[1;33m",
-
-  blue           => "\e[0;34m",
-  bblue          => "\e[1;34m",
-  "bold&blue"    => "\e[1;34m",
-  "blue&bold"    => "\e[1;34m",
-
-  purple         => "\e[0;35m",
-  bpurple        => "\e[1;35m",
-  "bold&purple"  => "\e[1;35m",
-  "purple&bold"  => "\e[1;35m",
-  magenta        => "\e[0;35m",
-  bmagenta       => "\e[1;35m",
-  "bold&magenta" => "\e[1;35m",
-  "magenta&bold" => "\e[1;35m",
-
-  cyan           => "\e[0;36m",
-  bcyan          => "\e[1;36m",
-  "bold&cyan"    => "\e[1;36m",
-  "cyan&bold"    => "\e[1;36m",
-
-  white          => "\e[0;37m",
-  bwhite         => "\e[1;37m",
-  gray           => "\e[1;37m",
-  grey           => "\e[1;37m",
-  "bold&white"   => "\e[1;37m",
-  "white&bold"   => "\e[1;37m",
-);
 
 sub parse_config_line
 {
