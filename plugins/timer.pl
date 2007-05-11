d91e6c4eb1ae2656fdf0e590a2455d80643c8c00
sartak <sartak@gmail.com>
2007-05-11 18:13:55 -0500

    Add my realtime clock to plugins

---
+++ plugins/timer.pl
@@ -0,0 +1,17 @@
+# adds a game clock to the right side of the penultimate line
+# also adds an extended command #time
+# by Eidolos
+
+our $start_time = time;
+
+each_iteration
+{
+  if ($in_game)
+  {
+    my $time = serialize_time(time - $start_time);
+    my $col = 81 - length $time;
+    $postprint .= "\e[s\e[23;${col}H\e[1;44m$time\e[0m\e[u";
+    s/(?:real)?timer?: unknown extended command/$time\e[K/g;
+  }
+}
+
