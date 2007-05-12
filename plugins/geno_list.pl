cdaa2690d6193b0de8b24a3e56e12675e4c2f7f5
sartak <sartak@gmail.com>
2007-05-11 19:33:42 -0500

    Add geno_list plugin, has some interesting code

---
+++ plugins/geno_list.pl
@@ -0,0 +1,39 @@
+# add extended-command #geno to list genocided species
+# by Eidolos
+
+my %geno;
+
+extended_command "#geno"
+              => sub
+                 {
+                     my $geno = join ', ', sort keys %geno;
+                     $geno = "No genocides." if $geno eq '';
+                     if (substr($&, 0, 3) eq "\e[H")
+                     {
+                       while (length($geno) > 70)
+                       {
+                         print "\e[H" . substr($geno, 0, 70, '') . "--More--\e[K";
+                         ReadKey(0);
+                       }
+                       $geno
+                     }
+                     else
+                     {
+                       if (length($geno) >= 30)
+                       {
+                         substr($geno, 0, 30) . "..."
+                       }
+                       else
+                       {
+                         $geno
+                       }
+                     }
+                 };
+each_iteration
+{
+  while (/You drop (.*?)\./g)
+  {
+    $geno{$1} = 1;
+  }
+}
+
