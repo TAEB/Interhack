acfea46369b77e62427c1647da5003a93a9d6679
jluehrs2 <jluehrs2@uiuc.edu>
2007-05-12 12:50:36 -0500

    another weird update pattern... thanks toft

--- plugins/no_stats.pl
+++ plugins/no_stats.pl
@@ -15,22 +15,24 @@ extended_command "#stats"
 
 each_iteration
 {
-  s{\e\[23;1H((.*?)\s*)(St:(\S+) Dx:(\d+) Co:(\d+) In:(\d+) Wi:(\d+) Ch:(\d+))(\s+(\w+)\s+)S:(\d+)}{
-    my $stat_start = length $1;
-    $char = $2;
-    $stats = $3;
-    @stats = ($4, $5, $6, $7, $8, $9);
+  my $s = qr/(?:\s|\e\[C)/;
+  my $st_char = qr#[\d\*/]#;
+  s{\e\[23;(\d+)H((.*?)$s*)(St:($st_char+) Dx:(\d+) Co:(\d+) In:(\d+) Wi:(\d+) Ch:(\d+))($s+(\w+)$s+)S:(\d+)}{
+    my $stat_start = length($2) + $1 - 1;
+    $char = $3;
+    $stats = $4;
+    @stats = ($5, $6, $7, $8, $9, ${10});
     $stat_pos[0] = $stat_start + 3;
     for my $i (1..5) {
       $stat_pos[$i] = $stat_pos[$i - 1] + length($stats[$i - 1]) + 4;
     }
-    $score_pos = $stat_start + length($stats) + length(${10}) + 2;
-    $align = ${11};
-    $score = ${12};
+    $score_pos = $stat_start + length($stats) + length(${11}) + 2;
+    $align = ${12};
+    $score = ${13};
     "\e[23;1HS:$score\e[K"
   }eg;
 
-  s{\e\[23;(?!1H)(\d+)H([\d\*/]+)}{
+  s{\e\[23;(?!1H)(\d+)H($st_char+)}{
     my ($update_pos, $update) = ($1, $2);
     for my $i (0..4) {
       if ($update_pos >= $stat_pos[$i] && $update_pos < $stat_pos[$i + 1]) {
