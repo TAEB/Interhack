7ea35369b09be2b2fcba1c5d33cb7c247788439e
jordanthelewis <jordanthelewis@gmail.com>
2007-05-12 00:34:13 -0500

    Adde ^Hd empty_^Hwand_name.pl to add a tab that automatically names wands where Nothing happens. to empty.

---
+++ plugins/emptywand_name.pl
@@ -0,0 +1,7 @@
+# Adds a tab that names wands empty if Nothing happens. there are a few false
+# positives, but they make no sense, and since they're hard to fix the user can
+# just not press tab. (Cursed bell of opening inside of a monster, rubbing
+# lamps.)
+# By toft
+
+make_tab qr/\e\[HNothing happens\./ => sub{"#name\ny${lastkey}empty\n"};
