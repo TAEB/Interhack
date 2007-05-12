b5d203abc66ac9b6666c529f70aaa9eacfcaa27e
jluehrs2 <jluehrs2@uiuc.edu>
2007-05-12 03:27:45 -0500

    monitor stat updates as well as score updates

--- plugins/no_stats.pl
+++ plugins/no_stats.pl
@@ -2,22 +2,51 @@
 # stats are still available through new #stats ext-cmd
 # by Eidolos
 
+my $char = '';
 my $stats = '';
+my @stats = ();
+my $align = '';
+my $score = '';
+my @stat_pos = ();
+my $score_pos = '';
 
 extended_command "#stats"
               => sub { $stats };
 
 each_iteration
 {
-  s{\e\[23;1H((.*?)\s*(St:\d+.*?Ch:\d+)\s+(\w+)\s+S:)(\d+)}{
-    $score_pos = length $1;
-    ($char, $stats, $align, $score) = ($2, $3, $4, $5);
+  s{\e\[23;1H((.*?)\s*)(St:(\w+) Dx:(\d+) Co:(\d+) In:(\d+) Wi:(\d+) Ch:(\d+))(\s+(\w+)\s+)S:(\d+)}{
+    my $stat_start = length $1;
+    $char = $2;
+    $stats = $3;
+    @stats = ($4, $5, $6, $7, $8, $9);
+    $stat_pos[0] = $stat_start + 3;
+    for my $i (1..5) {
+      $stat_pos[$i] = $stat_pos[$i - 1] + length($stats[$i - 1]) + 4;
+    }
+    $score_pos = $stat_start + length($stats) + length(${10}) + 2;
+    $align = ${11};
+    $score = ${12};
     "\e[23;1HS:$score\e[K"
   }eg;
-  s{\e\[23;(?!1H)(\d+)H(\d+)}{
-    my ($update_pos, $score_update) = ($1, $2);
-    my $same_length = $update_pos - $score_pos - 1;
-    substr($score, $same_length, length($score_update), $score_update);
+
+  s{\e\[23;(?!1H)(\d+)H([\d\*/]+)}{
+    my ($update_pos, $update) = ($1, $2);
+    for my $i (0..4) {
+      if ($update_pos >= $stat_pos[$i] && $update_pos < $stat_pos[$i + 1]) {
+        my $same_length = $update_pos - $stat_pos[$i] - 1;
+        substr($stats[$i], $same_length, length($update), $update);
+      }
+    }
+    if ($update_pos >= $stat_pos[5] && $update_pos < $score_pos) {
+      my $same_length = $update_pos - $stat_pos[5] - 1;
+      substr($stats[5], $same_length, length($update), $update);
+    }
+    elsif ($update_pos >= $score_pos) {
+      my $same_length = $update_pos - $score_pos - 1;
+      substr($score, $same_length, length($update), $update);
+    }
+    $stats = "St:$stats[0] Dx:$stats[1] Co:$stats[2] In:$stats[3] Wi:$stats[4] Ch:$stats[5]";
     "\e[23;1HS:$score\e[K"
   }eg;
 }
