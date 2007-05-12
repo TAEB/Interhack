f975b77861ee7a8aa85b18dcd20fdaf7adec8a07
jluehrs2 <jluehrs2@uiuc.edu>
2007-05-12 01:12:49 -0500

    parse the entire stats line

--- plugins/no_stats.pl
+++ plugins/no_stats.pl
@@ -9,7 +9,16 @@ extended_command "#stats"
 
 each_iteration
 {
-  $stats = $&
-    if s/\bSt:\d+.*?Ch:\d+/' ' x length $&/eg;
+  s{\e\[23;1H((.*?)\s*(St:\d+.*?Ch:\d+)\s+(\w+)\s+S:)(\d+)}{
+    $score_pos = length $1;
+    ($char, $stats, $align, $score) = ($2, $3, $4, $5);
+    "\e[23;1HS:$score\e[K"
+  }eg;
+  s{\e\[23;(?!1H)(\d+)H(\d+)}{
+    my ($update_pos, $score_update) = ($1, $2);
+    my $same_length = $update_pos - $score_pos - 1;
+    substr($score, $same_length, length($score_update), $score_update);
+    "\e[23;1HS:$score\e[K"
+  }eg;
 }
 
