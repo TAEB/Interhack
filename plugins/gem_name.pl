edef6fc30b0720fa633daa5e6642809a625bd70a
sartak <sartak@gmail.com>
2007-05-12 20:30:01 -0500

    Typo fix in gem_name improvement

--- plugins/gem_name.pl
+++ plugins/gem_name.pl
@@ -2,5 +2,5 @@
 # By toft
 
 make_tab qr/\e\[HYou write in the dust with (?:an?|\d+) (\w+) gems?\./ => sub{" \e#name\nn${lastkey}soft $1\n"};
-make_tab qr/\e\[HYou engrave in the floor with (?:an?|\d+) (\w+) gem?s\./ => sub{" \e#name\nn${lastkey}hard $1\n"};
+make_tab qr/\e\[HYou engrave in the floor with (?:an?|\d+) (\w+) gems?\./ => sub{" \e#name\nn${lastkey}hard $1\n"};
 
