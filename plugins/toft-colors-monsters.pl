62e7f0dcd7d7c4e25225839e4e9aa2127e942418
jordanthelewis <jordanthelewis@gmail.com>
2007-05-13 01:54:37 -0500

    Started working on monster colors

---
+++ plugins/toft-colors-monsters.pl
@@ -0,0 +1,28 @@
+# team a {{{
+recolor qr/giant ants?/ => "brown";
+recolor qr/killer bees?/ => "yellow";
+recolor qr/soldier ants?/ => "blue";
+recolor qr/fire ants?/ => "red";
+recolor qr/giant beetles?/ => "gray";
+recolor qr/queen bees?/ => "purple";
+# }}}
+
+# demons & {{{
+recolor qr/Death|Pestilence|Famine/ => "purple";
+recolor qr/Orcus|Baalzebub|Asmodeus/ => "purple";
+recolor qr/Juiblex/ => "bgreen";
+recolor qr/Yeenoghu|Geryon|Dispater|Demogorgon/ => "purple";
+
+recolor qr/djinni?/ => "yellow";
+recolor qr/balrogs?|pit fiends?|nalfeshnees?|horned devils?/ => "red";
+recolor qr/barbed devils?|mariliths?|vrocks?|hezrous?/ => "red";
+recolor qr/(?:succub|incub)(?:us|i)|sandestins?|bone devils?/ => "gray";
+recolor qr/ice devils?/ => "white";
+recolor qr/mail daemons?/ => "bblue";
+recolor qr/water demons?/ => "blue";
+# }}}
+
+# ghosts and shades X {{{
+recolor qr/ghosts?|shades?/ => "white";
+# }}}
+
