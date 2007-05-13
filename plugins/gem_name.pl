75e2d7d8fd0e232bece12fd8573c2c6e22008902
jordanthelewis <jordanthelewis@gmail.com>
2007-05-13 18:03:02 -0500

    Fixed gem_name to allow for yellowish browns and in-shop engraves.

--- plugins/gem_name.pl
+++ plugins/gem_name.pl
@@ -1,6 +1,6 @@
 # This adds tabs for easy gem naming based on hardness
 # by toft
 
-make_tab qr/\e\[HYou write in the dust with (?:an?|\d+) (\w+) gems?\./ => sub{" \e#name\nn$lastkeys[-1]soft $1\n"};
-make_tab qr/\e\[HYou engrave in the floor with (?:an?|\d+) (\w+) gems?\./ => sub{" \e#name\nn$lastkeys[-1]hard $1\n"};
+make_tab qr/\e\[HYou write in the dust with (?:an?|\d+) ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{" \e#name\nn$lastkeys[-1]soft $1\n"};
+make_tab qr/\e\[HYou engrave in the floor with (?:an?|\d+) ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{" \e#name\nn$lastkeys[-1]hard $1\n"};
 
