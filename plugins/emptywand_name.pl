712cda15c27ee27d79678e5e3bc71978912017ae
jordanthelewis <jordanthelewis@gmail.com>
2007-05-13 00:34:28 -0500

    Fixed emptywand_name to remove false positives

--- plugins/emptywand_name.pl
+++ plugins/emptywand_name.pl
@@ -1,7 +1,22 @@
-# Adds a tab that names wands empty if Nothing happens. there are a few false
-# positives, but they make no sense, and since they're hard to fix the user can
-# just not press tab. (Cursed bell of opening inside of a monster, rubbing
-# lamps.)
-# By toft
+# Adds a tab that names wands empty if Nothing happens.
+# by toft (but mostly Eidolos)
 
-make_tab qr/\e\[HNothing happens\./ => sub{"#name\ny$lastkeys[-1]empty\n"};
+each_iteration
+{
+  if (/\e\[HNothing happens\./)
+  {
+    my $skip = 1;
+    my $cmdkey;
+    for my $c (reverse @lastkeys)
+    {
+      next unless --$skip < 0;
+      $cmdkey = $c, last if $c =~ /[a-zA-Z]/;
+    }
+    annotate($cmdkey);
+    if ($cmdkey eq 'z')
+    {
+      tab("#name\ny$lastkeys[-1]empty\n");
+    }
+  }
+}
+
