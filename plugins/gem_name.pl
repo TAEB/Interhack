6d5c44d0f21f7efe0250678767eaef25f99c5c5e
sartak <sartak@gmail.com>
2007-05-12 20:26:21 -0500

    Fix problem in gem_name plugin which occurred when engraving with multiple gems in a stack

--- plugins/gem_name.pl
+++ plugins/gem_name.pl
@@ -1,6 +1,6 @@
 # This adds tabs for easy gem naming based on hardness
 # By toft
 
-make_tab qr/\e\[HYou write in the dust with an? (\w+) gem\./ => sub{" \e#name\nn${lastkey}soft $1\n"};
-make_tab qr/\e\[HYou engrave in the floor with an? (\w+) gem\./ => sub{" \e#name\nn${lastkey}hard $1\n"};
+make_tab qr/\e\[HYou write in the dust with (?:an?|\d+) (\w+) gems?\./ => sub{" \e#name\nn${lastkey}soft $1\n"};
+make_tab qr/\e\[HYou engrave in the floor with (?:an?|\d+) (\w+) gem?s\./ => sub{" \e#name\nn${lastkey}hard $1\n"};
 
