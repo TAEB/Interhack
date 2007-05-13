307333c06b2f6975fc4e55aef44d02e24b9d914b
jordanthelewis <jordanthelewis@gmail.com>
2007-05-13 00:34:37 -0500

    decapitalized by line in gem_name

--- plugins/gem_name.pl
+++ plugins/gem_name.pl
@@ -1,5 +1,5 @@
 # This adds tabs for easy gem naming based on hardness
-# By toft
+# by toft
 
 make_tab qr/\e\[HYou write in the dust with (?:an?|\d+) (\w+) gems?\./ => sub{" \e#name\nn$lastkeys[-1]soft $1\n"};
 make_tab qr/\e\[HYou engrave in the floor with (?:an?|\d+) (\w+) gems?\./ => sub{" \e#name\nn$lastkeys[-1]hard $1\n"};
