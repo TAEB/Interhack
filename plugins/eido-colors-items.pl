000f2b22f154c5f9ef7ad0b80b6c36c684bcea75
jluehrs2 <jluehrs2@uiuc.edu>
2007-05-13 19:25:04 -0500

    typo fixes

--- plugins/eido-colors-items.pl
+++ plugins/eido-colors-items.pl
@@ -242,11 +242,11 @@ if (!$ece_useless)
     recolor qr/ring (?:of|called) (?:adornment|hunger|protection(?: from shape changers)?|stealth|sustain ability|warning|aggravate monster|\w+ resistance|gain \w+|increase \w+|invisibility|see invisible|searching|polymorph(?: control)?)\b/ => $ec_useless_rings || $ec_useless || "darkgray" unless $ece_useless_rings;
 # }}}
 # wands {{{
-    recolor qr/wand (?:called|of) (?:light|nothing|locking|make invisible|opening|probing|secret door detection|\w+ monster|undead turning|create monster)\b/ => $ec_useless_wands || $ec_useless || "darkgray" unless $ece_useless_rings;
+    recolor qr/wand (?:called|of) (?:light|nothing|locking|make invisible|opening|probing|secret door detection|\w+ monster|undead turning|create monster)\b/ => $ec_useless_wands || $ec_useless || "darkgray" unless $ece_useless_wands;
 # }}}
 # amulets {{{
-    recolor qr/amulet (?:called|of) (?:versus poison|change|ESP|magical breathing|restful sleep|strangulation|unchanging)\b/ => $ec_useless_amulets || $ec_useless || "darkgray" unless $ece_useless_rings;
-    recolor qr/amulet versus poison\b/ => $ec_useless_amulets || $ec_useless || "darkgray" unless $ece_useless_rings;
+    recolor qr/amulet (?:called|of) (?:versus poison|change|ESP|magical breathing|restful sleep|strangulation|unchanging)\b/ => $ec_useless_amulets || $ec_useless || "darkgray" unless $ece_useless_amulets;
+    recolor qr/amulet versus poison\b/ => $ec_useless_amulets || $ec_useless || "darkgray" unless $ece_useless_amulets;
 # }}}
 }
 # }}}
@@ -260,4 +260,3 @@ if (!$ece_unid_armor)
 }
 # }}}
 # }}}
-
