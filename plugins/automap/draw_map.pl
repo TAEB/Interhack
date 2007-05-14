a59cbe2d3a16b374b35377be528ae3712471f8fa
sartak <sartak@gmail.com>
2007-05-14 00:04:21 -0500

    Make draw_map more for standalone running (for finding offsets)

--- plugins/automap/draw_map.pl
+++ plugins/automap/draw_map.pl
@@ -2,18 +2,28 @@
 use strict;
 use warnings;
 
+my ($y, $x) = (1, 1);
+my $set = 0;
+my $file = shift or die "Need a filename";
+($set, $y) = (1, shift) if @ARGV;
+($set, $x) = (1, shift) if @ARGV;
+push @ARGV, $file;
+
+print "\e[$y;${x}H\e[1;30m";
+
 while (<>)
 {
   chomp;
 
   if ($. == 1 && /^\s*(\d+)\s*(\d+)\s*$/)
   {
-    print "\e[$1;$2H";
+    print "\e[$1;$2H" unless $set;
     next;
   }
 
   my $length = length;
-  s/(\.+)/"\e[".length($1).'C'/eg;
+  s/([. +S]+)/"\e[".length($1).'C'/eg;
   print "$_\e[B\e[${length}D";
 }
+print "\e[0m";
 
