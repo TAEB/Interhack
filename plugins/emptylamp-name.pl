9d6cf4e32ef45bb28660b4560157414d770fef04
jordanthelewis <jordanthelewis@gmail.com>
2007-05-15 03:02:40 -0500

    fixed spacing issue in emptylamp-name

--- plugins/emptylamp-name.pl
+++ plugins/emptylamp-name.pl
@@ -1,5 +1,5 @@
 # Adds a tab that names lamps empty appropriately
 # by toft
 
-make_tab qr/\e\[HThis (?:oil )?lamp .*? has no oil\./ => sub{" \e#name\ny$lastkeys[-1]empty\n"};
-make_tab qr/\e\[HYour lamp .*? has run out of power\./ => sub{" \e#name\ny$lastkeys[-1]empty\n"};
+make_tab qr/\e\[HThis (?:oil )?lamp.*? has no oil\./ => sub{" \e#name\ny$lastkeys[-1]empty\n"};
+make_tab qr/\e\[HYour lamp.*? has run out of power\./ => sub{" \e#name\ny$lastkeys[-1]empty\n"};
