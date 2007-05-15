a6ac480817634a3e17e5c2fb9fa78a4c8b2ab68f
sartak <sartak@gmail.com>
2007-05-15 18:47:10 -0500

    Make timer disappear on menus that take up all rows

--- plugins/timer.pl
+++ plugins/timer.pl
@@ -9,9 +9,13 @@ each_iteration
   if ($in_game)
   {
     my $time = serialize_time(time - $start_time);
-    my $col = 81 - length $time;
-    $postprint .= "\e[s\e[23;${col}H\e[1;30m$time\e[0m\e[u";
     s/(?:real)?timer?: unknown extended command/$time\e[K/g;
+
+    my $col = 81 - length $time;
+    my $bottom = $vt->row_text(24);
+
+    $postprint .= "\e[s\e[23;${col}H\e[1;30m$time\e[0m\e[u"
+      unless $bottom =~ /\(\d+ of \d+\)/ || $bottom =~ /\(end\)/;
   }
 }
 
