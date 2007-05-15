2cb3716d131d5cb32f4fc779a1d366b6d406fa84
jordanthelewis <jordanthelewis@gmail.com>
2007-05-15 09:13:54 -0500

    added #donate: prints how much it is for clairvoyance and protection. so you can figure out how much you need before #chatting etc

--- plugins/donate.pl
+++ plugins/donate.pl
@@ -1,7 +1,11 @@
 # provides a shortcut for donating xl * 400 gold to priests
 # if you want clairvoyance, just divide by 2 >_>
-# by Eidolos (idea by toft)
+# by Eidolos and toft (idea by toft)
 
+extended_command "#donate"
+      => sub
+      {
+          "clairvoyance: \$" . $level * 200 . ", protection: \$" . $level * 400 . "\n"
+      };
 make_tab qr/\e\[HHow much will you offer\?/
       => sub { $level * 400 . "\n" };
-
