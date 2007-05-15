b76435c9eaedbee85d327f20e997eb5a0d8991b3
jordanthelewis <jordanthelewis@gmail.com>
2007-05-15 02:22:01 -0500

    Added touchstone tab

--- plugins/gem_name.pl
+++ plugins/gem_name.pl
@@ -14,3 +14,5 @@ my %hardgems = (
 make_tab qr/\e\[HYou write in the dust with (?:an?|\d+) ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{" \e#name\nn$lastkeys[-1]soft $1\n"};
 make_tab qr/\e\[HYou engrave in the floor with (?:an?|\d+) ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{my $name = exists($hardgems{$1}) ? $hardgems{$1} : "hard $1"; " \e#name\nn$lastkeys[-1]$name\n"};
 
+# adds "scritch scritch" sensing (useless? XXX)
+make_tab qr/\e\[H"scritch, scritch"/ => sub{my $keys = join('', reverse @lastkeys); $keys =~ /\W*(.)/; " \e#name\nn${1}touchstone\n"};
