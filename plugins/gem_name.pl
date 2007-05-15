a98a65470b55ba35a968c822083f212822f85517
jordanthelewis <jordanthelewis@gmail.com>
2007-05-15 01:54:52 -0500

    gem_names now names unique hard gems their real names instead of hard a_color

--- plugins/gem_name.pl
+++ plugins/gem_name.pl
@@ -1,6 +1,16 @@
 # This adds tabs for easy gem naming based on hardness
 # by toft
 
+my %hardgems = (
+    white             => 'diamond',
+    red               => 'ruby',
+    orange            => 'jacinth',
+    blue              => 'sapphire/aquamarine',
+    black             => 'black opal',
+    green             => 'emerald/aquamarine',
+    'yellowish brown' => 'topaz',
+);
+
 make_tab qr/\e\[HYou write in the dust with (?:an?|\d+) ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{" \e#name\nn$lastkeys[-1]soft $1\n"};
-make_tab qr/\e\[HYou engrave in the floor with (?:an?|\d+) ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{" \e#name\nn$lastkeys[-1]hard $1\n"};
+make_tab qr/\e\[HYou engrave in the floor with (?:an?|\d+) ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{my $name = exists($hardgems{$1}) ? $hardgems{$1} : "hard $1"; " \e#name\nn$lastkeys[-1]$name\n"};
 
