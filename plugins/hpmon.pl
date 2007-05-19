61adeeb61930ace2171ae1cda77b2111906bd2e0
jluehrs2 <jluehrs2@uiuc.edu>
2007-05-19 11:30:52 -0500

    update hpmon to use new botl parsing code

--- plugins/hpmon.pl
+++ plugins/hpmon.pl
@@ -3,18 +3,19 @@
 # it also doesn't "miss" like the real hpmon
 # by Eidolos
 
+include "stats";
+
 each_iteration
 {
-  s{(\e\[24;\d+H|HP:)((\d+)\((\d+)\))}{
+  $botl{hp} =~ s{(HP:)(\d+\(\d+\))}{
     my $color = '';
-    ($curhp, $maxhp) = ($3, $4);
 
-       if ($3 * 7 <= $4 || $3 <= 6) { $color = "\e[1;30m" }
-    elsif ($3     >= $4)            {                     }
-    elsif ($3 * 2 >= $4)            { $color = "\e[1;32m" }
-    elsif ($3 * 3 >= $4)            { $color = "\e[1;33m" }
-    elsif ($3 * 4 >= $4)            { $color = "\e[0;31m" }
-    elsif ($3 * 5 >= $4)            { $color = "\e[1;31m" }
+       if ($curhp * 7 <= $maxhp || $curhp <= 6) { $color = "\e[1;30m" }
+    elsif ($curhp     >= $maxhp)            {                     }
+    elsif ($curhp * 2 >= $maxhp)            { $color = "\e[1;32m" }
+    elsif ($curhp * 3 >= $maxhp)            { $color = "\e[1;33m" }
+    elsif ($curhp * 4 >= $maxhp)            { $color = "\e[0;31m" }
+    elsif ($curhp * 5 >= $maxhp)            { $color = "\e[1;31m" }
 
     "$1$color$2\e[0m"
   }eg;
