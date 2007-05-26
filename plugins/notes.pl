1d1cdb7023df382a8f4a7e1d6662761d485ee8be
sartak <sartak@gmail.com>
2007-05-26 13:52:31 -0500

    Add a bunch of autonotes (stores and altars are pretty much done)

--- plugins/notes.pl
+++ plugins/notes.pl
@@ -62,5 +62,46 @@ extended_command "#notes" # {{{
                      request_redraw();
 
                      return "Thank you sir, may I have another?";
-                 };
+                 }; # }}}
+
+# set up some autonotes (this will become its own plugin) {{{
+# major events {{{
+note_all "\e[HFor what do you wish?" => "Got a wish!";
+note_all qr/Welcome to experience level (\d+)\./ => sub { "Hit experience level $1." };
+# }}}
+# altars {{{
+note_once qr/\e\[HThere is an altar to .*? \((\w+)\) here\./ => sub { "\u$1 altar" };
+note_once qr/\e\[H.\s+.*?\((\w+) altar\)/ => sub { "\u$1 altar" };
+# }}}
+# shops {{{
+# when we get a generic shop message, we check $shop{dlvl} -- if it's already
+# set to >0, then we have seen a more specific shop message
+our %shop;
+
+note_once qr/\e\[HYou hear (?:the chime of a cash register|someone cursing shoplifters)\./ => sub { $shop{$dlvl} ? "" : "Level has some kind of shop"};
+
+# get rid of "again" when persistence is in..
+our %shoptype =
+(
+    'general store' => 'general',
+    'used armor dealership' => 'armor',
+    'second-hand bookstore' => 'scroll',
+    'liquor emporium' => 'potion',
+    'antique weapons outlet' => 'weapon',
+    'delicatessen' => 'food',
+    'jewelers' => 'ring',
+    'quality apparel and accessories' => 'wand',
+    'hardware store' => 'tool',
+    'rare books' => 'spellbook',
+    'lighting store' => 'lighting',
+);
+my $shops = join '|', keys %shoptype;
+
+note_once qr/\e\[(?:\d+;1)?H"[^\e]+Welcome(?: again)? to .*? ($shoptype)!"/
+       => sub { $shop{$dlvl} = 1; "Level has a $shoptype{$1} store"};
+# }}}
+# nonshop special rooms {{{
+note_once qr/You hear the footsteps of a guard on patrol\./ => "Level has a vault";
+# }}}
+# }}}
 
