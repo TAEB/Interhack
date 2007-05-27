17d0b534079cbd87c90945f734f5f90f81a0a99e
sartak <sartak@gmail.com>
2007-05-26 21:15:14 -0500

    Expand wish-menu a bunch by stealing a lot of items from doy, and allow align-specific wishes (mostly for quest arts)

--- plugins/wish-menu.pl
+++ plugins/wish-menu.pl
@@ -6,16 +6,74 @@
 
 our $wish_quantity = 2 unless defined $wish_quantity;
 our $wish_enchantment = 3 unless defined $wish_enchantment;
+our $bfg = "blessed fixed greased";
+our $armor = "$bfg +$wish_enchantment";
 
 our %wishes =
 (
-    A => sub { "$wish_quantity blessed scrolls of charging" },
-    B => 'uncursed fixed greased magic marker',
-    C => sub { "blessed fixed greased +$wish_enchantment silver dragon scale mail" },
-    D => 'blessed fixed greased +3 helm of brilliance',
-    E => 'blessed fixed greased ring of conflict',
-    F => 'blessed fixed greased ring of levitation',
+    A => "$wish_quantity blessed scrolls of charging",
+    B => "$wish_quantity blessed scrolls of genocide",
+    C => "uncursed fixed greased magic marker",
+
+    D => "$armor gray dragon scale mail",
+    E => "$armor silver dragon scale mail",
+    F => "$armor speed boots",
+    G => "$armor jumping boots",
+    H => "$armor helm of brilliance",
+    I => "$armor helm of telepathy",
+    J => "$armor helm of opposite alignment",
+    K => "$armor gauntlets of power",
+
+    L => "$bfg ring of conflict",
+    M => "$bfg ring of levitation",
+    N => "$bfg ring of teleport control",
+
+    O => "$armor Grayswandir",
+    P => "$wish_quantity cursed potions of gain level",
+    Q => "$bfg amulet of life saving",
+    R => "7 $bfg candles",
+    S => "$bfg bag of holding",
 );
+
+our %law_wishes =
+(
+    T => "$armor Sceptre of Might",
+    U => "$bfg Magic Mirror of Merlin",
+    V => "$bfg Orb of Detection",
+);
+
+our %neu_wishes =
+(
+    T => "$bfg Eye of the Aethiopica",
+    U => "$bfg Orb of Fate",
+    V => "$bfg Platinum Yendorian Express Card",
+);
+
+our %cha_wishes =
+(
+    T => "$bfg Master Key of Thievery",
+    U => "$bfg spellbook of identify",
+    V => "$bfg spellbook of magic mapping",
+);
+
+my $added_align = '';
+
+each_iteration
+{
+    return if $added_align eq $align;
+    my %align_wishes;
+
+       if ($align eq 'Law') { %align_wishes = %law_wishes }
+    elsif ($align eq 'Neu') { %align_wishes = %neu_wishes }
+    elsif ($align eq 'Cha') { %align_wishes = %cha_wishes }
+
+    while (my ($letter, $wish) = each(%align_wishes))
+    {
+        $wishes{$letter} = $wish;
+    }
+
+    $added_align = $align;
+}
 
 show_menu qr/^For what do you wish\? +$/ => \%wishes;
 
