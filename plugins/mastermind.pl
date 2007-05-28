48c68c5faae17a55cf4dfea69d7466f5b4b4b023
jluehrs2 <jluehrs2@uiuc.edu>
2007-05-28 01:48:53 -0500

    change mastermind to use each_match instead of each_iteration

--- plugins/mastermind.pl
+++ plugins/mastermind.pl
@@ -10,40 +10,41 @@ extended_command "#mm"
               => sub { ($responses_so_far, $response_this_play) = ('', 1);
                        "Resetting Mastermind status." };
 
-each_iteration
-{
-  if (/\e\[(?:(?:1;)?\d+)?HYou hear (\d) tumblers? click and (\d) gears? turn\./)
-  {
-    $responses_so_far .= " $2$1";
-    $response_this_play = 1;
-  }
-  elsif (/\e\[(?:(?:1;)?\d+)?HYou hear (\d) tumblers? click\./)
-  {
-    $responses_so_far .= " 0$1";
-    $response_this_play = 1;
-  }
-  elsif (/\e\[(?:(?:1;)?\d+)?HYou hear (\d) gears? turn\./)
-  {
-    $responses_so_far .= " ${1}0";
-    $response_this_play = 1;
-  }
-  elsif (/\e\[HWhat tune are you playing\?/)
-  {
-    $responses_so_far .= " 00" unless $response_this_play;
-    $response_this_play = 0;
+each_match qr/\e\[(?:(?:1;)?\d+)?HYou hear (\d) tumblers? click and (\d) gears? turn\./
+    => sub
+       {
+           $responses_so_far .= " $2$1";
+           $response_this_play = 1;
+       };
+each_match qr/\e\[(?:(?:1;)?\d+)?HYou hear (\d) tumblers? click\./
+    => sub
+       {
+           $responses_so_far .= " 0$1";
+           $response_this_play = 1;
+       };
+each_match qr/\e\[(?:(?:1;)?\d+)?HYou hear (\d) gears? turn\./
+    => sub
+       {
+           $responses_so_far .= " ${1}0";
+           $response_this_play = 1;
+       };
+each_match qr/\e\[HWhat tune are you playing\?/
+    => sub
+       {
+           $responses_so_far .= " 00" unless $response_this_play;
+           $response_this_play = 0;
 
-    my $next = `$mastermind_prog $responses_so_far`;
-    if ($next =~ /ACK/)
-    {
-      ($responses_so_far, $response_this_play) = ('', 1);
-      annotate("No possible tunes. Resetting. Press tab for AABBC\\n.");
-      $tab = "AABBC\n";
-    }
-    else
-    {
-      ($next) = $next =~ /^([A-G]{5})/;
-      tab("$next\n");
-    }
-  }
-}
+           my $next = `$mastermind_prog $responses_so_far`;
+           if ($next =~ /ACK/)
+           {
+               ($responses_so_far, $response_this_play) = ('', 1);
+               annotate("No possible tunes. Resetting. Press tab for AABBC\\n.");
+               $tab = "AABBC\n";
+           }
+           else
+           {
+               ($next) = $next =~ /^([A-G]{5})/;
+               tab("$next\n");
+           }
+       };
 
