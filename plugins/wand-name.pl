30d8ff81136daf90683fcdd3f2221626539434dc
sartak <sartak@gmail.com>
2007-05-28 13:03:50 -0500

    Make wand-name use the VT

--- plugins/wand-name.pl
+++ plugins/wand-name.pl
@@ -12,10 +12,10 @@ my $nw = sub
     return $sub;
 };
 
-make_tab qr/\e\[HThe engraving now reads: ".*?"./ => sub{my $key = alphakeys(-1); " \e#name\nn${key}polymorph\n"};
-make_tab qr/\e\[HThe wand unsuccessfully fights your attempt to write!/ => $nw->("striking");
-make_tab qr/\e\[HA few ice cubes drop from the wand./ => $nw->("cold");
-make_tab qr/\e\[HThe \w+ is riddled by bullet holes!/ => $nw->("magic missile");
-make_tab qr/\e\[HThe bugs on the \w+ slow down!/ => $nw->("slow monster");
-make_tab qr/\e\[HThe bugs on the \w+ speed up!/ => $nw->("speed monster");
+make_tab_vt qr/^The engraving now reads: ".*?"./ => sub{my $key = alphakeys(-1); " \e#name\nn${key}polymorph\n"};
+make_tab_vt qr/^The wand unsuccessfully fights your attempt to write!/ => $nw->("striking");
+make_tab_vt qr/^A few ice cubes drop from the wand./ => $nw->("cold");
+make_tab_vt qr/^The \w+ is riddled by bullet holes!/ => $nw->("magic missile");
+make_tab_vt qr/^The bugs on the \w+ slow down!/ => $nw->("slow monster");
+make_tab_vt qr/^The bugs on the \w+ speed up!/ => $nw->("speed monster");
 
