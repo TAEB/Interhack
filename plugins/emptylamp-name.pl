48c4d1b7e0524eb6c27d9b2b5391650f3e8976f9
jluehrs2 <jluehrs2@uiuc.edu>
2007-05-28 04:54:48 -0500

    make the string used for empty items configurable

--- plugins/emptylamp-name.pl
+++ plugins/emptylamp-name.pl
@@ -1,7 +1,9 @@
 # Adds a tab that names lamps empty appropriately
 # by toft
 
+our $empty ||= "empty";
+
 make_tab qr/\e\[HThis (?:oil )?lamp.*? has no oil\./
-    => sub{my $key = alphakeys(-1); " \e#name\ny${key}empty\n"};
+    => sub{my $key = alphakeys(-1); " \e#name\ny${key}$empty\n"};
 make_tab qr/\e\[HYour lamp.*? has run out of power\./
-    => sub{my $key = alphakeys(-1); " \e#name\ny${key}empty\n"};
+    => sub{my $key = alphakeys(-1); " \e#name\ny${key}$empty\n"};
--- plugins/emptywand-name.pl
+++ plugins/emptywand-name.pl
@@ -1,10 +1,12 @@
 # Adds a tab that names wands empty if Nothing happens.
 # by toft (but mostly Eidolos)
 
+our $empty ||= "empty";
+
 make_tab qr/Nothing happens\./
     => sub
        {
            return unless alphakeys(-2) eq 'z';
            my $wand_slot = alphakeys(-1);
-           "\e#name\ny${wand_slot}empty\n"
+           "\e#name\ny${wand_slot}$empty\n"
        }
