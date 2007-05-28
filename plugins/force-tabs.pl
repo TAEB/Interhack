da914b896fd3e9268feb41a25481ce953bf562f6
jordanthelewis <jordanthelewis@gmail.com>
2007-05-28 13:35:05 -0500

    Made turns to live more accurate - the uncertainty comes from variable moves per turn, not from variable turns

--- plugins/force-tabs.pl
+++ plugins/force-tabs.pl
@@ -2,13 +2,13 @@
 # like "You are slowing down."
 # by Eidolos and toft (idea by doy)
 
-press_tab(qr/\bYou are slowing down\./, "Lizard, !acid, STF, pray? 2-4 turns to live.");
+press_tab(qr/\bYou are slowing down\./, "Lizard, !acid, STF, pray? 2 turns to live.");
 press_tab(qr/\bYou don't feel very well\./, 'Fire, Cure Sickness, Aesc, "unchang, poly, pray? Around 10 turns to live.');
 press_tab qr/\bYou faint from lack of food\./;
 press_tab(qr/\bYou feel deathly sick\./, "You have about 20 turns to live.");
 press_tab(qr/\bYou feel (?:much|even) worse\./, "Your turns to live have just been cut to about a third.");
 press_tab qr/\bStop eating\?/;
-press_tab(qr/\bThe [^.!\e]*? swings itself around you!/, 'Kill, tele, poly, "MB, freeze, delev? 1-2 turns to live.');
+press_tab(qr/\bThe [^.!\e]*? swings itself around you!/, 'Kill, tele, poly, "MB, freeze, delev? 1 turn to live.');
 press_tab qr/^Really quit\? \[yn\] \(n\) +$/;
 
 press_tab qr/"So thou thought thou couldst kill me, fool\."/;
