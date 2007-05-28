0f08267678ea48d055757ab80cfaf0cd2685f213
jluehrs2 <jluehrs2@uiuc.edu>
2007-05-28 01:38:38 -0500

    use each_match instead of each_iteration in emptywand-name.pl

--- plugins/emptywand-name.pl
+++ plugins/emptywand-name.pl
@@ -1,21 +1,19 @@
 # Adds a tab that names wands empty if Nothing happens.
 # by toft (but mostly Eidolos)
 
-each_iteration
-{
-  if (/\e\[HNothing happens\./)
-  {
-    my $skip = 1;
-    my $cmdkey;
-    for my $c (reverse @lastkeys)
-    {
-      next unless --$skip < 0;
-      $cmdkey = $c, last if $c =~ /[a-zA-Z]/;
-    }
-    if ($cmdkey eq 'z')
-    {
-      tab("#name\ny$lastkeys[-1]empty\n");
-    }
-  }
-}
+each_match qr/\e\[HNothing happens\./
+    => sub
+       {
+           my $skip = 1;
+           my $cmdkey;
+           for my $c (reverse @lastkeys)
+           {
+               next unless --$skip < 0;
+               $cmdkey = $c, last if $c =~ /[a-zA-Z]/;
+           }
+           if ($cmdkey eq 'z')
+           {
+               tab("#name\ny$lastkeys[-1]empty\n");
+           }
+       };
 
