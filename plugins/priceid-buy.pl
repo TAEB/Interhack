7bce1b37f54f32cb4bdfef0fc8fd9adb68ee4cf5
sartak <sartak@gmail.com>
2007-05-28 13:03:42 -0500

    Make priceid-buy use the VT

--- plugins/priceid-buy.pl
+++ plugins/priceid-buy.pl
@@ -148,7 +148,7 @@ sub calc_base
     return map {int} ($amount, $amount_sur);
 }
 
-make_tab qr/\e\[H(?:You have a little trouble lifting )?(.) - (an?|\d+) (?:blessed |uncursed |cursed )?(.*?) ?(scroll|potion|wand|ring|spellbook)s? (?:(?:called |labeled )(.*?) ?)?\(unpaid, (\d+) zorkmids?\)\./
+make_tab_vt qr/^(?:You have a little trouble lifting )?(.) - (an?|\d+) (?:blessed |uncursed |cursed )?(.*?) ?(scroll|potion|wand|ring|spellbook)s? (?:(?:called |labeled )(.*?) ?)?\(unpaid, (\d+) zorkmids?\)\./
       => sub
          {
              my ($letter, $count, $appearance, $type, $existing, $cost)
