74a18bc4284a19e7ab7b54eb61293165576b5259
sartak <sartak@gmail.com>
2007-05-28 18:10:43 -0500

    Refactor the short_names for price IDing to do in code what it used to do in data, and only for multi-possibility items

--- plugins/priceid-buy.pl
+++ plugins/priceid-buy.pl
@@ -2,103 +2,72 @@ our %short_names;
 our %price_table =
 (
   'scroll' => {
-              20  => $short_names{scroll}{20} ||
-                         ['identify'],
-              50  => $short_names{scroll}{50} ||
-                         ['light'],
-              60  => $short_names{scroll}{60} ||
-                         ['enchant weapon'],
-              80  => $short_names{scroll}{80} ||
-                         ['enchant armor', 'remove curse'],
-              100 => $short_names{scroll}{100} ||
-                         ['confusion', 'destroy armor', 'fire',
-                          'food detection', 'gold detection', 'magic mapping',
-                          'scare monster', 'teleportation'],
-              200 => $short_names{scroll}{200} ||
-                         ['amnesia', 'create monster', 'earth', 'taming'],
-              300 => $short_names{scroll}{300} ||
-                         ['charging', 'genocide', 'punishment',
-                          'stinking cloud'],
+              20  => ['identify'],
+              50  => ['light'],
+              60  => ['enchant weapon'],
+              80  => ['enchant armor', 'remove curse'],
+              100 => ['confusion', 'destroy armor', 'fire',
+                      'food detection', 'gold detection', 'magic mapping',
+                      'scare monster', 'teleportation'],
+              200 => ['amnesia', 'create monster', 'earth', 'taming'],
+              300 => ['charging', 'genocide', 'punishment',
+                      'stinking cloud'],
           },
   'spellbook' => {
-              100 => $short_names{spellbook}{100} ||
-                         ['detect monsters', 'force bolt', 'healing', 'jumping',
-                          'knock', 'light', 'protection', 'sleep'],
-              200 => $short_names{spellbook}{200} ||
-                         ['confuse monster', 'create monster', 'cure blindness',
-                          'detect food', 'drain life', 'magic missile',
-                          'slow monster', 'wizard lock'],
-              300 => $short_names{spellbook}{300} ||
-                         ['cause fear', 'charm monster', 'clairvoyance',
-                          'cure sickness', 'detect unseen', 'extra healing',
-                          'haste self', 'identify', 'remove curse',
-                          'stone to flesh'],
-              400 => $short_names{spellbook}{400} ||
-                         ['cone of cold', 'detect treasure', 'fireball',
-                          'invisibility', 'levitation', 'restore ability'],
-              500 => $short_names{spellbook}{500} ||
-                         ['dig', 'magic mapping'],
-              600 => $short_names{spellbook}{600} ||
-                         ['create familiar', 'polymorph', 'teleport away',
-                          'turn undead'],
-              700 => $short_names{spellbook}{700} ||
-                         ['cancellation', 'finger of death'],
+              100 => ['detect monsters', 'force bolt', 'healing', 'jumping',
+                      'knock', 'light', 'protection', 'sleep'],
+              200 => ['confuse monster', 'create monster', 'cure blindness',
+                      'detect food', 'drain life', 'magic missile',
+                      'slow monster', 'wizard lock'],
+              300 => ['cause fear', 'charm monster', 'clairvoyance',
+                      'cure sickness', 'detect unseen', 'extra healing',
+                      'haste self', 'identify', 'remove curse',
+                      'stone to flesh'],
+              400 => ['cone of cold', 'detect treasure', 'fireball',
+                      'invisibility', 'levitation', 'restore ability'],
+              500 => ['dig', 'magic mapping'],
+              600 => ['create familiar', 'polymorph', 'teleport away',
+                      'turn undead'],
+              700 => ['cancellation', 'finger of death'],
           },
   'potion' => {
-              0   => $short_names{potion}{0} ||
-                         ['water'],
-              50  => $short_names{potion}{50} ||
-                         ['booze', 'fruit juice', 'see invisible', 'sickness'],
-              100 => $short_names{potion}{100} ||
-                         ['confusion', 'extra healing', 'hallucination',
-                          'healing', 'restore ability', 'sleeping', 'water'],
-              150 => $short_names{potion}{150} ||
-                         ['blindness', 'gain energy', 'invisibility',
-                          'monster detection', 'object detection'],
-              200 => $short_names{potion}{200} ||
-                         ['enlightenment', 'full healing', 'levitation',
-                          'polymorph', 'speed'],
-              250 => $short_names{potion}{250} ||
-                         ['acid', 'oil'],
-              300 => $short_names{potion}{300} ||
-                         ['gain ability', 'gain level', 'paralysis'],
+              0   => ['water'],
+              50  => ['booze', 'fruit juice', 'see invisible', 'sickness'],
+              100 => ['confusion', 'extra healing', 'hallucination',
+                      'healing', 'restore ability', 'sleeping', 'water'],
+              150 => ['blindness', 'gain energy', 'invisibility',
+                      'monster detection', 'object detection'],
+              200 => ['enlightenment', 'full healing', 'levitation',
+                      'polymorph', 'speed'],
+              250 => ['acid', 'oil'],
+              300 => ['gain ability', 'gain level', 'paralysis'],
           },
   'ring' => {
-              100 => $short_names{ring}{100} ||
-                         ['adornment', 'hunger', 'protection',
-                          'protection from shape changers', 'stealth',
-                          'sustain ability', 'warning'],
-              150 => $short_names{ring}{150} ||
-                         ['aggravate monster', 'cold resistance',
-                          'gain constitution', 'gain strength',
-                          'increase accuracy', 'increase damage',
-                          'invisibility', 'poison resistance',
-                          'see invisible', 'shock resistance'],
-              200 => $short_names{ring}{200} ||
-                         ['fire resistance', 'free action', 'levitation',
-                          'regeneration', 'searching', 'slow digestion',
-                          'teleportation'],
-              300 => $short_names{ring}{300} ||
-                         ['conflict', 'polymorph', 'polymorph control',
-                          'teleport control'],
+              100 => ['adornment', 'hunger', 'protection',
+                      'protection from shape changers', 'stealth',
+                      'sustain ability', 'warning'],
+              150 => ['aggravate monster', 'cold resistance',
+                      'gain constitution', 'gain strength',
+                      'increase accuracy', 'increase damage',
+                      'invisibility', 'poison resistance',
+                      'see invisible', 'shock resistance'],
+              200 => ['fire resistance', 'free action', 'levitation',
+                      'regeneration', 'searching', 'slow digestion',
+                      'teleportation'],
+              300 => ['conflict', 'polymorph', 'polymorph control',
+                      'teleport control'],
           },
   'wand' => {
-              0   => $short_names{wand}{0} ||
-                         ['uncharged'],
-              100 => $short_names{wand}{100} ||
-                         ['light', 'nothing'],
-              150 => $short_names{wand}{150} ||
-                         ['digging', 'enlightenment', 'locking',
-                          'magic missile', 'make invisible', 'opening',
-                          'probing', 'secret door detection', 'slow monster',
-                          'speed monster', 'striking', 'undead turning'],
-              175 => $short_names{wand}{175} ||
-                         ['cold', 'fire', 'lightning', 'sleep'],
-              200 => $short_names{wand}{200} ||
-                         ['cancellation', 'create monster', 'polymorph',
-                          'teleportation'],
-              500 => $short_names{wand}{500} ||
-                         ['death', 'wishing'],
+              0   => ['uncharged'],
+              100 => ['light', 'nothing'],
+              150 => ['digging', 'enlightenment', 'locking',
+                      'magic missile', 'make invisible', 'opening',
+                      'probing', 'secret door detection', 'slow monster',
+                      'speed monster', 'striking', 'undead turning'],
+              175 => ['cold', 'fire', 'lightning', 'sleep'],
+              200 => ['cancellation', 'create monster', 'polymorph',
+                      'teleportation'],
+              500 => ['death', 'wishing'],
           },
 );
 
@@ -175,12 +144,14 @@ make_tab qr/^(?:You have a little trouble lifting )?(.) - (an?|\d+) (?:blessed |
              $existing = " $existing" if $existing ne '';
 
              my ($price1, $price2) = calc_base($ch, $cost);
+             my $possibilities = 0;
 
              for ($price1, map {$price1 - $_, $price1 + $_} 1..4)
              {
                  if (exists $price_table{$type}{$_})
                  {
                      $price1 = $_;
+                     $possibilities += @{$price_table{$type}{$_}};
                      last;
                  }
              }
@@ -190,13 +161,25 @@ make_tab qr/^(?:You have a little trouble lifting )?(.) - (an?|\d+) (?:blessed |
                  if (exists $price_table{$type}{$_})
                  {
                      $price2 = $_;
+                     $possibilities += @{$price_table{$type}{$_}};
                      last;
                  }
              }
 
-             my @possibilities
-                   = map {@{$price_table{$type}{$_}}}
-                     $price1, $price2;
+             my @possibilities;
+             if ($possibilities == 1)
+             {
+                @possibilities = map {@{$price_table{$type}{$_}}}
+                                 $price1, $price2;
+             }
+             else
+             {
+               @possibilities = map {@{
+                                         $short_names{$type}{$_}
+                                      || $price_table{$type}{$_}
+                                     }}
+                                 $price1, $price2;
+             }
 
              $cost = join '/',
                      grep {exists $price_table{$type}{$_}}
