189fd7d53df0213889d17b3141fd22b14761bebb
sartak <sartak@gmail.com>
2007-07-01 15:21:39 -0500

    Add magic-marker scribbles to Elbereth, and add regex for "adding" text

--- plugins/elbereth.pl
+++ plugins/elbereth.pl
@@ -3,3 +3,7 @@
 
 make_tab qr/^What do you want to (?:write|engrave|burn)(?! (?:wi|on))/ => "Elbereth\n";
 
+make_tab qr/^What do you want to scribble on / => "Elbereth\n";
+
+make_tab qr/^What do you want to add to the (?:writing|engraving|text burned|graffiti)/ => "Elbereth\n";
+
