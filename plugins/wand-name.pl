b5b8d4d1b7461a46a1bad10a1fc6019693c9a199
Shawn M Moore <sartak@gmail.com>
2007-10-10 19:49:06 -0500

    Some cleanups to the wand patch

--- plugins/wand-name.pl
+++ plugins/wand-name.pl
@@ -3,41 +3,40 @@
 # Assumes the user is engraving over a previous engraving,
 # and only engraves one letter
 #
-# by toft
-#
-# $getwand added by Alex, adds support multi-character engravings
+# by toft and alexbobp
 
-my $getwand = sub
+sub getwand
 {
-    for (my $i=-2; $i>=-30; --$i)
+    for my $i (reverse -30 .. -2)
     {
-        if ((alphakeys($i) eq "y") || (alphakeys($i) eq "n"))
+        if (alphakeys($i) eq "y" || alphakeys($i) eq "n")
         {
-            my $j=$i-2;
-            while ((alphakeys($j) eq "?") || (alphakeys($j) eq "*") || (alphakeys($j) eq " "))
+            if (alphakeys($i - 2) eq "E")
             {
-                $j--;
-            }
-            if (alphakeys($j) eq "E")
-            {
-                return alphakeys($i-1);
+                return alphakeys($i - 1);
             }
         }
     }
-    return "-";
-};
 
-my $nw = sub
+    return "-";
+}
+
+sub nw
 {
     my $name = shift;
-    my $sub = sub{my $key = $getwand->(); ($key eq "-" ? "" : " \e#name\nn${key}${name}\n")};
-    return $sub;
-};
+    return sub
+    {
+        my $key = getwand;
+        return $key eq "-"
+               ? ""
+               : " \e#name\nn${key}${name}\n"
+    }
+}
 
 make_tab qr/^The engraving now reads: ".*?"./ => sub{my $key = alphakeys(-1); " \e#name\nn${key}polymorph\n"};
-make_tab qr/^The wand unsuccessfully fights your attempt to write!/ => $nw->("striking");
-make_tab qr/^A few ice cubes drop from the wand./ => $nw->("cold");
-make_tab qr/^The \w+ is riddled by bullet holes!/ => $nw->("magic missile");
-make_tab qr/^The bugs on the \w+ slow down!/ => $nw->("slow monster");
-make_tab qr/^The bugs on the \w+ speed up!/ => $nw->("speed monster");
+make_tab qr/^The wand unsuccessfully fights your attempt to write!/ => nw("striking");
+make_tab qr/^A few ice cubes drop from the wand./ => nw("cold");
+make_tab qr/^The \w+ is riddled by bullet holes!/ => nw("magic missile");
+make_tab qr/^The bugs on the \w+ slow down!/ => nw("slow monster");
+make_tab qr/^The bugs on the \w+ speed up!/ => nw("speed monster");
 
