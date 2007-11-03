a2d09d00c27472b900008599f884aac0e4930b5e
Shawn M Moore <sartak@gmail.com>
2007-11-03 01:12:35 -0500

    Add a #map command that will figure out what map should be drawn for your level

--- plugins/automap.pl
+++ plugins/automap.pl
@@ -3,10 +3,16 @@
 # the Gazetteer ought to help
 # by Eidolos
 
+my %maps;
+
 sub read_map
 {
-    my $file = shift;
+    my $name = shift;
+    return $maps{$name} if exists $maps{$name};
+
     my @output;
+
+    my $file = "plugins/automap/$name.txt";
 
     open(my $handle, '<', $file)
       or return undef;
@@ -25,6 +31,8 @@ sub read_map
         push @output, $_;
     }
 
+    $maps{$name} = \@output
+        if @output > 2;
     return \@output;
 }
 
@@ -38,66 +46,152 @@ sub p {
 sub draw_map
 {
     my $mapfile = shift;
-    sub
+    if (!-e "plugins/automap/$mapfile.txt")
     {
-        if (!-e "plugins/automap/$mapfile.txt")
-        {
-            return "Sorry, plugins/automap/$mapfile.txt doesn't exist";
+        return "Sorry, plugins/automap/$mapfile.txt doesn't exist";
+    }
+
+    my $map = read_map($mapfile);
+
+    if (!defined($map) || @$map == 0)
+    {
+        return "Sorry, can't read plugins/automap/$mapfile.txt";
+    }
+
+    my ($y, $X);
+
+    p "\e[1;30m";
+
+    for my $row (@$map) {
+        if (ref($row) eq 'ARRAY') {
+            ($y, $X) = @$row;
+            next;
         }
 
-        my $map = read_map("plugins/automap/$mapfile.txt");
+        p "\e[$y;${X}H";
+        ++$y;
+        my $x = $X;
+        for my $tile (split '', $row) {
+            ++$x;
 
-        if (!defined($map) || @$map == 0)
-        {
-            return "Sorry, can't read plugins/automap/$mapfile.txt";
-        }
-
-        my ($y, $X) = @{ shift @$map };
-
-        p "\e[1;30m";
-
-        for my $row (@$map) {
-            p "\e[$y;${X}H";
-            ++$y;
-            my $x = $X;
-            for my $tile (split '', $row) {
-                ++$x;
-
-                # skip if tile is empty
-                if ($tile eq ' ') {
-                    p "\e[C";
-                }
-                # skip if there's already something on the map
-                elsif (substr($vt->row_plaintext($y-1), $x-2, 1) ne ' ') {
-                    p "\e[C";
-                }
-                # otherwise add it, my good man
-                else {
-                   p $tile;
-                }
+            # skip if tile is empty
+            if ($tile eq ' ') {
+                p "\e[C";
+            }
+            # skip if there's already something on the map
+            elsif (substr($vt->row_plaintext($y-1), $x-2, 1) ne ' ') {
+                p "\e[C";
+            }
+            # otherwise add it, my good man
+            else {
+                p $tile;
             }
         }
-
-        p "\e[m";
-
-        "Drawing at (y$y, x$X). Press ^R to redraw the screen."
     }
+
+    p "\e[m";
+
+    "Drawing $mapfile at (y$y, x$X). Press ^R to redraw the screen."
 }
 
-for (qw/frontier townsquare alley college grotto bustling bazaar
-        oracle ludios castle valley sanctum
-        cellar catacombs mimic
-        bigplus bigoval bigtie
-        samfill
-        medusa1 medusa2
-        asmodeus baalzebub juiblex orcus
-        wiztop wizmid wizbot
-        vladtop vladmid vladbot
-        fakewiz
-        earth air fire astral
-       /, map {("${_}home", "${_}loc", "${_}goal")}
-          qw/arc bar cav hea kni mon pri ran rog sam tou val wiz/)
+my @maps = qw/frontier townsquare alley college grotto bustling bazaar
+              oracle ludios castle valley sanctum
+              cellar catacombs mimic
+              bigplus bigoval bigtie
+              samfill
+              medusa1 medusa2
+              asmodeus baalzebub juiblex orcus
+              wiztop wizmid wizbot
+              vladtop vladmid vladbot
+              fakewiz
+              earth air fire astral
+             /, map {("${_}home", "${_}loc", "${_}goal")}
+                qw/arc bar cav hea kni mon pri ran rog sam tou val wiz/;
+
+for my $name (@maps)
 {
-    extended_command $_ => draw_map($_);
+    extended_command $name => sub { draw_map($name) };
+}
+
+extended_command 'map' => sub { guess_and_draw_map(\@maps) };
+
+sub guess_and_draw_map
+{
+    my $available = shift;
+    my @best = (0);
+    for my $name (@$available) {
+        my $map = read_map($name);
+        my ($correct, $checked) = match_map($vt, $map);
+
+        next if $checked < 10;
+
+        my $match = int(100 * $correct / $checked);
+
+        @best = ($match, $name, $correct, $checked)
+            if $match > $best[0];
+
+        next if $match < 95;
+
+        draw_map($name);
+        return "The $name map matched your current level: "
+             . "$match% ($correct/$checked)";
+    }
+
+    return "You need to explore more of the map." if $best[0] == 0;
+    return "No map matched your level. "
+         . "The best was $best[1] with $best[0]% ($best[2]/$best[3]).";
+}
+
+sub match_map
+{
+    my $vt = shift;
+    my $map = shift;
+
+    my $correct = 0;
+    my $checked = 0;
+
+    my ($X, $y);
+
+    for (@$map) {
+        my $row = $_;
+
+        if (ref($row) eq 'ARRAY') {
+            ($y, $X) = @$row;
+            next;
+        }
+
+        ++$y;
+        my $x = $X;
+
+        # replace all monsters and items with .
+        $row =~ s{[a-zA-Z~!@\$%^&*()+=<>/\[\]"':;\\]}{.}g;
+
+        # replace walls and corridors
+        $row =~ y/-|#/ww./;
+
+        for my $tile (split '', $row) {
+            ++$x;
+
+            my $visible = substr($vt->row_plaintext($y-1), $x-2, 1);
+
+            # haven't explored this spot or it's not part of the defined map
+            if ($visible eq ' ' || $tile eq ' ') {
+                next;
+            }
+
+            # replace all monsters and items with .
+            $visible =~ s{[a-zA-Z~!@\$%^&*()+=<>/\[\]"':;\\]}{.}g;
+
+            # replace walls and corridors
+            $visible =~ y/-|#/ww./;
+
+            ++$checked;
+            if ($visible eq $tile) {
+                ++$correct;
+            }
+        }
+    }
+
+    return ($correct, $checked);
 }
 
