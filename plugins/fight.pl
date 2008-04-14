f38dd2d9d71dd2d1a2bf95732b0e41a87328c4af
jluehrs2 <jluehrs2@uiuc.edu>
2008-04-14 17:32:20 -0500

    support numpad in fight.pl

--- plugins/fight.pl
+++ plugins/fight.pl
@@ -5,25 +5,49 @@
 our $fight_enabled = 0;
 
 sub enable_fight {
-    remap h => 'Fh';
-    remap j => 'Fj';
-    remap k => 'Fk';
-    remap l => 'Fl';
-    remap y => 'Fy';
-    remap u => 'Fu';
-    remap b => 'Fb';
-    remap n => 'Fn';
+    if ($vikeys) {
+        remap h => 'Fh';
+        remap j => 'Fj';
+        remap k => 'Fk';
+        remap l => 'Fl';
+        remap y => 'Fy';
+        remap u => 'Fu';
+        remap b => 'Fb';
+        remap n => 'Fn';
+    }
+    else {
+        remap 1 => 'F1';
+        remap 2 => 'F2';
+        remap 3 => 'F3';
+        remap 4 => 'F4';
+        remap 6 => 'F6';
+        remap 7 => 'F7';
+        remap 8 => 'F8';
+        remap 9 => 'F9';
+    }
 }
 
 sub disable_fight {
-    unmap 'h';
-    unmap 'j';
-    unmap 'k';
-    unmap 'l';
-    unmap 'y';
-    unmap 'u';
-    unmap 'b';
-    unmap 'n';
+    if ($vikeys) {
+        unmap 'h';
+        unmap 'j';
+        unmap 'k';
+        unmap 'l';
+        unmap 'y';
+        unmap 'u';
+        unmap 'b';
+        unmap 'n';
+    }
+    else {
+        unmap 1;
+        unmap 2;
+        unmap 3;
+        unmap 4;
+        unmap 6;
+        unmap 7;
+        unmap 8;
+        unmap 9;
+    }
 }
 
 extended_command "#fight"
