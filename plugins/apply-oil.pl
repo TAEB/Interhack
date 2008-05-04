f471719affad54e6181b561d805c07f3376253e2
Shawn M Moore <sartak@gmail.com>
2008-05-04 01:46:22 -0500

    Remove apply oil, it's terrible

--- plugins/apply-oil.pl
+++
@@ -1,8 +0,0 @@
-# sets up a tab to apply new potions to test for oil
-# if the potion IS oil the #name will have no real effect
-# it really should only match once per potion appearance
-# by Eidolos
-
-make_tab qr/^(.) - (?:an?|\d+)? (?:blessed |uncursed |cursed )?(?!clear)([\w -]+) potions?\./
-      => sub { "\ea$1#name\nn$1$2 !oil\n" };
-
