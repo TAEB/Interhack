6b6bae8a138201fc6fa7a824fed2693facf812f0
Stefan `Sec` Zehl <sec@42.org>
2012-11-10 20:15:41 +0100

    fix to work if gem is BUC-identified

--- plugins/gem-name.pl
+++ plugins/gem-name.pl
@@ -11,8 +11,8 @@ my %hardgems = (
     'yellowish brown' => 'topaz',
 );
 
-make_tab qr/^You write in the dust with (?:an?|\d+) ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{" \e#name\nn$lastkeys[-1]soft $1\n"};
-make_tab qr/^You engrave in the floor with (?:an?|\d+) ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{my $name = exists($hardgems{$1}) ? $hardgems{$1} : "hard $1"; " \e#name\nn$lastkeys[-1]$name\n"};
+make_tab qr/^You write in the dust with (?:an?|\d+)(?: uncursed|cursed|blessed)? ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{" \e#name\nn$lastkeys[-1]soft $1\n"};
+make_tab qr/^You engrave in the floor with (?:an?|\d+)(?: uncursed|cursed|blessed)? ([\w ]+) gems?(?: \(unpaid, \d+ zorkmids?\))?\./ => sub{my $name = exists($hardgems{$1}) ? $hardgems{$1} : "hard $1"; " \e#name\nn$lastkeys[-1]$name\n"};
 
 # adds "scritch scritch" sensing (useless? XXX)
 make_tab '"scritch, scritch"' => sub{my $key = alphakeys(-2); " \e#name\nn${key}touchstone\n"};
